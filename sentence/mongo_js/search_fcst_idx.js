f6220f549c12571f6f39bf6a645521fe9bda56bc
markwh1te <iamwh1temark@outlook.com>
2017-08-24 09:07:07 +0800

    update

--- sentence/mongo_js/search_fcst_idx.js
+++ sentence/mongo_js/search_fcst_idx.js
@@ -1,3 +1,4 @@
+//  order sign
 var i = 1
 var fields_list = ['trade_date','symbol','mkt_idx.tcap'];
 var fields_dict = fields_list.map(x=>({x:{'$exist':false}}))
@@ -14,17 +15,3 @@ db.getCollection('Z3_EQUITY_HISTORY').find(
     search_dict,
     {}
 ).sort({'trade_date':i}).count()
-
-
-
-// db.getCollection('Z3_EQUITY_HISTORY').find(
-// {
-//     // 'symbol':/^3/,
-//     'trade_date':{
-//          '$gte':ISODate('20070101'),
-//          '$lte':ISODate('20170728')
-//     }
-//     ''
-//      },{
-//      }
-// ).sort({'trade_date':i})
